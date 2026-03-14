06a97b278fbe550ab027111b9760067c325c214d
Valeria Suarez Moreno <valesmuniversidad@gmail.com>
2026-03-13 22:19:38 -0500

    3

--- functions.py
+++ functions.py
@@ -1,10 +1,11 @@
 
 def promedio_estudiante(calificaciones):
     if len(calificaciones) == 0:
-        return 0,0
-    return sum(calificaciones) / len(calificaciones) 
+        return 0.0
+    resultado = sum(calificaciones) / len(calificaciones)
+    return resultado
 
-calificaciones =[85, 92, 78]     
+calificaciones =[]  
 promedio = promedio_estudiante(calificaciones)
 print(promedio)
 
