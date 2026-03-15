0c56fcc5ab8fc55d954e48cc243a23c0b9d4b150
Valeria Suarez Moreno <valesmuniversidad@gmail.com>
2026-03-15 17:14:56 -0500

    5

--- return_types.py
+++ return_types.py
@@ -1,11 +1,26 @@
 #def obtener_precio_usuario():
-   #valor = input(float("Enter the item's price:\n"))
+   
    #return valor
         
+#def obtener_precio_usuario(precio):
+   # precio= (obtener_precio_usuario)
+    #return precio
+##precio=[0.0]
+#lor = obtener_precio_usuario(precio)
+
+#rint("Enter the item's price:\n")
+#alor= input(float)
+#rint(valor)
+#ef obtener_precio_usuario(precio):
+precio= []
 def obtener_precio_usuario(precio):
-    precio= float(input(obtener_precio_usuario))
-    return precio
-precio=[75]
+    if len(precio) == 0:
+        return 0.0
+    sino =precio
+    print("Enter the item's price:\n")
+    return sino
 
 valor = obtener_precio_usuario(precio)
 print(valor)
+   
+
