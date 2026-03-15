c3f44f5c8c7a33c4544516c851b93fa5ca244567
Valeria Suarez Moreno <valesmuniversidad@gmail.com>
2026-03-15 16:33:38 -0500

    4

---
+++ return_types.py
@@ -0,0 +1,11 @@
+#def obtener_precio_usuario():
+   #valor = input(float("Enter the item's price:\n"))
+   #return valor
+        
+def obtener_precio_usuario(precio):
+    precio= float(input(obtener_precio_usuario))
+    return precio
+precio=[75]
+
+valor = obtener_precio_usuario(precio)
+print(valor)
