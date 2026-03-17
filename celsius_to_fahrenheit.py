567c1e9df4134794eb7cb82aca6854312e5a6879
Valeria Suarez Moreno <valesmuniversidad@gmail.com>
2026-03-16 23:02:45 -0500

    27

---
+++ celsius_to_fahrenheit.py
@@ -0,0 +1,6 @@
+def celsius_to_fahrenheit(temp):
+    temperatura = (temp * 9/5) + 32
+    return temperatura
+print(celsius_to_fahrenheit(0))    # Salida esperada: 32.0
+print(celsius_to_fahrenheit(100))  # Salida esperada: 212.0
+print(celsius_to_fahrenheit(-40))  # Salida esperada: -40.0
